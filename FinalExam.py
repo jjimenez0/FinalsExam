857142a8b4d14d90d7445c65cebe55249b5e96ab
jjimenez0 <157805479+jjimenez0@users.noreply.github.com>
2024-05-16 14:39:51 +0800

    Update FinalExam.py

--- FinalExam.py
+++ FinalExam.py
@@ -35,5 +35,5 @@ else:
     prediction=import_and_predict(img,model)
     #class_names=['T-shirt', 'Trouser', 'Pullover', 'Dress','Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Shoe']
     class_names=['0', '1', '2', '3','4','5', '6', '7', '8', '9']
-    string="OUTPUT : "+ class_names[np.argmax(prediction)]
+    string="OUTPUT : "+ class_names[np.argmax(prediction[0])]
     st.success(string)
