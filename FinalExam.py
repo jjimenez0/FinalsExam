72f171cd5107d6ce4673352ebe588e560abd01b2
jjimenez0 <157805479+jjimenez0@users.noreply.github.com>
2024-05-16 14:38:02 +0800

    Update FinalExam.py

--- FinalExam.py
+++ FinalExam.py
@@ -25,7 +25,7 @@ def import_and_predict(img,model):
     img = img.astype('float32')
     img = img / 255.0
     predict = np.argmax(model.predict(img), axis=1)
-    result = predict[]
+    #result = predict[]
     return result
 
 if file is None:
@@ -36,5 +36,5 @@ else:
     prediction=import_and_predict(img,model)
     #class_names=['T-shirt', 'Trouser', 'Pullover', 'Dress','Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Shoe']
     class_names=['0', '1', '2', '3','4','5', '6', '7', '8', '9']
-    string="OUTPUT : "+ class_names[result]
+    string="OUTPUT : "+ class_names[result[0]]
     st.success(string)
